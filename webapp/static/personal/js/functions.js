5880ece52f0d01aadfda812e72a040b6ff48339c
vijaykumardev <vijayvk.it@gmail.com>
2018-07-23 16:58:49 -0600

    Added Table and Charts on all page

--- webapp/static/personal/js/functions.js
+++ webapp/static/personal/js/functions.js
@@ -45,6 +45,94 @@ function exportTableToCSV(filename, idSelector) {
   downloadCSV(csv.join("\n"), filename);
 }
 
+function convertChartLine(renderAt,idName,type,chartID) {
+  var getTabularData = function() {
+    var table = document.getElementById(idName),
+      rows = table.children[0].children,
+      row,
+      i,
+      length,
+      data = [];
+    // get the table element and iterate over its children and extract the data
+    for (i = 1, length = rows.length; i < length; i++) {
+
+      row = rows[i];
+      data.push({
+        label: row.children[0].innerHTML,
+        value: row.children[1].innerHTML
+      });
+    }
+
+    return data;
+  };
+  var revenueChart = new FusionCharts({
+    type: type,
+    renderAt: renderAt+"-render",
+    width: '1000',
+    height: '450',
+    dataFormat: 'json',
+    id: 'line-chart',
+    dataSource: {
+      "chart": {
+        "caption": $("#"+renderAt).attr("title"),
+        "subCaption": "",
+        "xAxisName":$("#"+renderAt).attr("x-axis"),
+        "yAxisName": $("#"+renderAt).attr("y-axis"),
+        "numberPrefix": "",
+        "theme": "fint",
+        "rotateValues": "1",
+        "exportEnabled": "1"
+      },
+      "data": getTabularData()
+    }
+  });
+  revenueChart.render();
+}
+function convertChartBar(renderAt,idName) {
+    var getTabularData = function() {
+    var table = document.getElementById(idName),
+      rows = table.children[0].children,
+      row,
+      i,
+      length,
+      data = [];
+    // get the table element and iterate over its children and extract the data
+    for (i = 1, length = rows.length; i < length; i++) {
+
+      row = rows[i];
+      data.push({
+        label: row.children[0].innerHTML,
+        value: row.children[1].innerHTML
+      });
+    }
+
+    return data;
+  };
+  var revenueChart2D = new FusionCharts({
+    type: 'column2D',
+    renderAt: renderAt,
+    width: '1000',
+    height: '450',
+    dataFormat: 'json',
+    id: 'bar-chart',
+    dataSource: {
+      "chart": {
+        "caption": caption,
+        "subCaption": "",
+        "xAxisName": 'state',
+        "yAxisName": yAxisName,
+        "numberPrefix": "",
+        "theme": "fint",
+        "rotateValues": "1",
+        "exportEnabled": "1"
+      },
+      "data": getTabularData()
+    }
+  });
+  revenueChart2D.render();
+}
+
+
 /**
  * submit the form of a page when option is selected
  * * @param  {string}  formName     name of the form where the option is selected
@@ -58,15 +146,16 @@ $(document).ready(function() {
   //hide all divecontainers
   $('#collapsible-panels div').hide()
 
+  //convertChartBar();
   $('#collapsible-panels a').click(function(e) {
     var clicked = $(this).attr('id');
-    if (clicked == "graph2017")
-      convertChartLine17();
-    if (clicked == "line")
-      convertChartLine();
-    else
-      convertChartBar();
-
+    var chartType = $("#"+clicked).attr("type");
+    if (clicked == "line-2017"  || clicked == "bar-2017"){
+      convertChartLine(clicked,"dataTable17",chartType,"chart-line");
+    }
+    else if (clicked == "line-2009" || clicked == "bar-2009"){
+      convertChartLine(clicked,"dataTable",chartType,"chart-line");
+    }
 
     //slide down the corresponding div if hidden, or slide up if shown
     $(this).parent().next('#collapsible-panels div').slideToggle('slow');
